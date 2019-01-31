05f3d30fcd198ea9e4e2a1ec25d1f2753c763281
Feb29Dev <feb29dev@gmail.com>
2019-01-30 22:53:38 -0800

    adding initial formulae

---
+++ xmrig-macos.rb
@@ -0,0 +1,13 @@
+class XMRigMacOS < Formula
+    desc "XMRig build for MacOS"
+    homepage "https://github.com/feb29dev/xmrig"
+    url "https://github.com/feb29dev/xmrig/releases/download/v2.10.0/xmrig-2.10.0-macos.tar.gz"
+    sha256 "3f0cc7014ce31eaf36930f2a9afc8ff9306ab93a2741b5b5c6f7c49df907f6cb"
+    version "2.10.0"
+  
+    bottle :unneeded
+  
+    def install
+      bin.install "xmrig"
+    end
+  end
